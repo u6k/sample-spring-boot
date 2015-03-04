b8e8c4097836ba7082a6874444931e08d97170bf
Naono Yuichi <u6k.apps@gmail.com>
2015-03-04 19:23:53 +0900

    空プロジェクトを作成しました。

---
+++ sample-spring-boot/src/main/java/jp/gr/java_conf/u6k/sample_spring_boot/App.java
@@ -0,0 +1,13 @@
+package jp.gr.java_conf.u6k.sample_spring_boot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
