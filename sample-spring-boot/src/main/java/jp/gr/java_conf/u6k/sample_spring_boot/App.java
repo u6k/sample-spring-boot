af312cfc0ff4fbd844a0ebfb24a5001f00e44456
Naono Yuichi <u6k.apps@gmail.com>
2015-03-04 19:46:41 +0900

    単純にtext/plainを返すコントローラーを作成し、動作確認をしました。

--- sample-spring-boot/src/main/java/jp/gr/java_conf/u6k/sample_spring_boot/App.java
+++ sample-spring-boot/src/main/java/jp/gr/java_conf/u6k/sample_spring_boot/App.java
@@ -1,13 +1,23 @@
+
 package jp.gr.java_conf.u6k.sample_spring_boot;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class App {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String home() {
+        return "Hello, sample-spring-boot!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 }
